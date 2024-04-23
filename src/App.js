58baeaf57700b76fcb85089056a0593dc23ccc89
Tirth Keraliya <tirthkeraliya200@gmail.com>
2024-04-23 15:39:02 +0530

    add link ridrectthe url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { fetchActiveURL } from "./utils/api";
+import Background from "./images/homebg.png";
+import RightArrow from "./images/rightarrow.svg";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [activeURLs, setActiveURLs] = useState([]);
+  const [hasRedirected, setHasRedirected] = useState(false);
+  const [splashBackgroundImage, setSplashBackgroundImage] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchActiveURL();
+        setActiveURLs(data.items);
+
+        const splashImage = data.splashImage;
+        setSplashBackgroundImage(splashImage || "");
+
+        const activeURL = data.items.find((item) => item.isActive);
+
+        if (activeURL && !hasRedirected) {
+          window.open(activeURL.url, "_blank");
+          setHasRedirected(true);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [hasRedirected]);
+
+  const handleButtonClick = () => {
+    const activeURL = activeURLs.find((item) => item.isActive);
+
+    if (activeURL && activeURL.url) {
+      window.open(activeURL.url, "_blank");
+    }
+  };
+
   return (
-    <div className="App">
-      <h1>Testing App</h1>
-    </div>
+    <>
+      {loading ? (
+        <div className="center-wrap">
+          <p>Hang tight, we're getting things ready for you.</p>
+        </div>
+      ) : (
+        <div
+          className="main-wrap"
+          style={{
+            backgroundImage: `url(${splashBackgroundImage || Background})`,
+          }}
+        >
+          <div className="bottom-wrap">
+            <button className="btn-wrap" onClick={handleButtonClick}>
+              Click here to visit the casino
+              <img src={RightArrow} alt="right-arrow" />
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/utils/api.js
@@ -0,0 +1,19 @@
+export const fetchActiveURL = async () => {
+  const API_URL = "https://cdn.contentful.com";
+  const SPACE_ID = "1igtcus6456h";
+  const ACCESS_TOKEN = "JaSKyZvZwu4uByhPQc-p8MPRLUPLep-Dyk5hAkYz_tw";
+  const COLLECTION_NAME = "active-urls";
+  try {
+    const response = await fetch(
+      `${API_URL}/spaces/${SPACE_ID}/environments/master/entries?content_type=${COLLECTION_NAME}&access_token=${ACCESS_TOKEN}&limit=100`
+    );
+    const responseData = await response.json();
+    const items = responseData.items.map((item) => item.fields);
+    const splashImage = responseData.includes.Asset[0].fields.file.url;
+    console.log("Data from API:", responseData.includes.Asset[0].fields);
+    return { items, splashImage };
+  } catch (error) {
+    console.error("Error fetching data from API:", error);
+    throw error;
+  }
+};
