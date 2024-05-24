e7152710d2eda48c4eca064fa10cb19c8e8fb37c
Nilay <chahodiyanilay30@gmail.com>
2024-05-24 17:27:55 +0530

    update for mac build

--- public/notarize.js
+++ public/notarize.js
@@ -1,14 +1,21 @@
 require("dotenv").config();
-
 const { notarize } = require("@electron/notarize");
 
-exports.default = async function notarizing() {
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  const appName = context.packager.appInfo.productFilename;
+
+  if (electronPlatformName !== "darwin") {
+    return;
+  }
+
   try {
     await notarize({
+      appBundleId: "com.devservices.mrxbet",
+      appPath: `${appOutDir}/${appName}.app`,
       appleId: "test.wecode.inc@gmail.com",
       appleIdPassword: "frmb-fxzz-lkmt-suxc",
       teamId: "G6G549CHCQ",
-      appBundleId:"com.devservices.mrxbet",
     });
     console.log("Notarization successful!");
   } catch (error) {
